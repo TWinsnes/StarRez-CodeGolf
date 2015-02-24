3711bac45b29b7a26b5237a06ce8b51c4c2a6644
jamesformica <james.formica@gmail.com>
2015-02-25 09:50:34 +1100

    shorten a little bit thanks for Tom

--- 02.HelloWorld/james/helloworld_min.js
+++ 02.HelloWorld/james/helloworld_min.js
@@ -1,1 +1,1 @@
-var n=true+false,t=n<<n,i=t<<t,d=i*(n+t)+n<<t,e=d+n,l=d+i,_=i<<t,c=l-_*t,x=_+n,H=_+_+i,W=c+c-n,o=W+_-i,r=o+n+t;alert(String.fromCharCode(H,e,l,l,o,c,_,W,o,r,l,d,x));
+n=-~NaN,t=n<<n,i=t<<t,d=i*(n+t)+n<<t,e=d+n,l=d+i,_=i<<t,c=l-_*t,x=_+n,H=_+_+i,W=c+c-n,o=W+_-i,r=o+n+t;alert(String.fromCharCode(H,e,l,l,o,c,_,W,o,r,l,d,x))
